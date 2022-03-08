ac921f802ade632d4817a60716d00e3bec4988e7
nunoinfante <nunoinfante89@gmail.com>
2022-03-08 18:27:49 +0000

    projeto_1

---
+++ net_client.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Aplicações Distribuídas - Projeto 1 - net_client.py
+Grupo:
+Números de aluno:
+"""
+
+# zona para fazer importação
+
+import sock_utils as su
+
+# definição da classe server_connection 
+
+class server_connection:
+    """
+    Abstrai uma ligação a um servidor TCP. Implementa métodos para: estabelecer 
+    a ligação; envio de um comando e receção da resposta; terminar a ligação.
+    """
+    def __init__(self, address, port):
+        """
+        Inicializa a classe com parâmetros para funcionamento futuro.
+        """
+        self.address = address
+        self.port = port
+        self.sock = None
+        
+    def connect(self):
+        """
+        Estabelece a ligação ao servidor especificado na inicialização.
+        """
+        self.sock = su.create_tcp_client_socket(self.address, self.port)
+
+    def send_receive(self, data):
+        """
+        Envia os dados contidos em data para a socket da ligação, e retorna
+        a resposta recebida pela mesma socket.
+        """
+        self.sock.sendall(data)
+        resposta = self.sock.recv(1024)
+        return resposta
+
+    def close(self):
+        """
+        Termina a ligação ao servidor.
+        """
+        self.sock.close()
